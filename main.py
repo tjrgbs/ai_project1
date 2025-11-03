8e662053f164c9829ca38ef5dc57c239e1c23920
tjrgbs <twichwwjxjjs@gmail.com>
2025-11-03 13:02:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!!')
-st.text_input('이름을 임력하세요')
-st.button('인사말 생성')
+name=st.text_input('이름을 임력하세요')
+if st.button('인사말 생성'):
+  st.write(name+'니미! 안녕하세요!')
